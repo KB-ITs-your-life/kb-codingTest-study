b65a909073b2c3c52023f6235865c84a5f33ed4b
eunhyeon5322 <eunhyeon5322@gmail.com>
2025-04-07 19:32:29 +0900

    #5 BOJ1426->BOJ1436으로 변경

--- eunhyeon/week01/BOJ1436.java
+++ eunhyeon/week01/BOJ1436.java
@@ -2,7 +2,7 @@ package week01;
 
 import java.util.Scanner;
 
-public class BOJ1426 {
+public class BOJ1436 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
