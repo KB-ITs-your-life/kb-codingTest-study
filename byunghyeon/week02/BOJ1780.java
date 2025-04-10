f7de1f29d0effff4738902524fffcb6399931abd
강병현 <kim1228com@naver.com>
2025-04-10 12:38:41 +0900

    #10 BOJ1780 해결

--- byunghyeon/week02/BOJ1780.java
+++ byunghyeon/week02/BOJ1780.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BOJ1780 {
+public class BOJ1780 { // 해결
 
     static int[][] table;
     static int N;
