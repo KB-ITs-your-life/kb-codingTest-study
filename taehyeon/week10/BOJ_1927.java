9be288a8b7ecb48b436b79706b6462670f9bcb93
kirikaka <kiridodo0714@gmail.com>
2025-06-04 11:38:25 +0900

    #65 BOJ_1927 Solving

---
+++ taehyeon/week10/BOJ_1927.java
@@ -0,0 +1,28 @@
+package week10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class BOJ_1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int n=Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> heap=new PriorityQueue<>();
+        for(int i=0;i<n;i++){
+            int now=Integer.parseInt(br.readLine());
+            if(now==0){
+                if(heap.isEmpty()){
+                    System.out.println(0);
+                } else{
+                    System.out.println(heap.poll());
+                }
+
+            }
+            else{
+                heap.add(now);
+            }
+        }
+    }
+}
