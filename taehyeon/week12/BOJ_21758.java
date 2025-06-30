15276e379a5c2b15a554a34895768c36018b9954
kirikaka <kiridodo0714@gmail.com>
2025-06-30 13:38:38 +0900

    #75 14500 Solving

--- taehyeon/week12/BOJ_21758.java
+++ taehyeon/week12/BOJ_21758.java
@@ -3,6 +3,10 @@ package week12;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class BOJ_21758 {
     public static void main(String[] args) throws IOException {
@@ -34,7 +38,8 @@ public class BOJ_21758 {
             answer = Math.max(answer, temp);
         }
         System.out.println(answer);
+
     }
 }
 
-}
+
